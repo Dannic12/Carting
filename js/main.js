7db16a8f14f3e8876270992ef372967abc8547e3
Dannic12 <dannyx00new1@gmail.com>
2025-04-27 15:53:08 +0300

    added section blog/css-modal window and contacts/footer

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ $(function(){
   });
 
 });
+
+const blog__info = document.querySelector('.blog__info')
+const openModal = document.querySelector('.open-modal')
+const closeModal = document.querySelector('.close-modal')
+
+function openModalAndBlockScroll() {
+  blog__info.showModal()
+  document.body.classList.add('scroll-block')
+}
+
+function returnScroll() {
+  document.body.classList.remove('scroll-block')
+}
+
+function close() {
+  blog__info.close()
+  returnScroll()
+}
+
+openModal.addEventListener('click', openModalAndBlockScroll)
+closeModal.addEventListener('click', () => {})
